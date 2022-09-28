511bddc8f3fec040167d2c36dbe8e7a3ebbbfa15
Mattia Dalzocchio <mattiadalzocchio@me.com>
2022-09-28 13:45:38 +0200

    Fix building error

--- bundle.mjs
+++ bundle.mjs
@@ -1,9 +1,7 @@
 // @ts-check
-import { watch } from "chokidar"
 import esbuild from "esbuild"
 import fs from "node:fs"
 import path from "node:path"
-import ts from "typescript"
 
 // eslint-disable-next-line prettier/prettier
 import packageJson from "./package.json" assert { type: "json" }
@@ -29,7 +27,7 @@ if (fs.existsSync(DIST_PATH)) {
 
 await build()
 
-async function build() {
+export async function build() {
   await esbuild.build({
     entryPoints: entries,
     outdir: "dist",
@@ -46,49 +44,6 @@ async function build() {
   // await buildDefinitions()
 }
 
-async function buildDefinitions() {
-  const program = ts.createProgram({
-    rootNames: entries,
-    options: {
-      target: ts.ScriptTarget.ESNext,
-      module: ts.ModuleKind.ESNext,
-      moduleResolution: ts.ModuleResolutionKind.NodeJs,
-      lib: ["DOM", "DOM.Iterable", "ESNext"],
-      types: ["node"],
-      declaration: true,
-      declarationMap: true,
-      emitDeclarationOnly: true,
-      outDir: "dist",
-      esModuleInterop: true,
-      forceConsistentCasingInFileNames: true,
-      strict: true,
-      strictNullChecks: true,
-      noImplicitAny: true,
-      noImplicitReturns: true,
-      noFallthroughCasesInSwitch: true,
-      noUncheckedIndexedAccess: true,
-      skipLibCheck: true,
-      skipDefaultLibCheck: true,
-      sourceMap: false,
-    },
-  })
-  const emitResult = program.emit()
-
-  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics)
-
-  allDiagnostics.forEach(diagnostic => {
-    if (diagnostic.file) {
-      const { line, character } = ts.getLineAndCharacterOfPosition(
-        diagnostic.file,
-        diagnostic.start ?? 0
-      )
-      const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")
-      console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`)
-    }
-  })
-}
-
-
 // Copy & edit package.json
 
 /** @type {Record<string, any>} */
@@ -101,13 +56,3 @@ delete packageCopy.optionalDependencies
 delete packageCopy.pnpm
 
 fs.writeFileSync(path.join(DIST_PATH, "package.json"), Buffer.from(JSON.stringify(packageCopy, null, 2), "utf-8"))
-
-// Watch in dev mode
-
-const args = process.argv
-if (!args.includes("-w")) process.exit(0)
-
-console.log("Watching for file changes...")
-
-const watcher = watch(path.resolve("src"), { persistent: true })
-watcher.on("add", build).on("change", build).on("unlink", build)
