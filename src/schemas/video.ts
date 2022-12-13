d14b98177d691e311b73891dc4e4ec08a62f39f3
Mattia Dalzocchio <mattiadalzocchio@me.com>
2022-12-13 09:49:07 +0100

    update video schema

--- src/schemas/video.ts
+++ src/schemas/video.ts
@@ -5,69 +5,121 @@ import { ImageRawSchema, ImageSchema } from "./image"
 
 const quality = z.custom<`${number}p`>(val => /^\d+p$/g.test(val as string))
 
-export const VideoSourceRawSchema = z.object({
-  /** Video resolution (eg: 1080p) */
-  quality: quality,
-  /** Swarm reference of the video */
-  reference: beeReference,
-  /** Video size in bytes */
-  size: z.number().min(0),
-  /** Video bitrate */
-  bitrate: z.number().min(0),
-})
+export const VideoSourceRawSchema = z.discriminatedUnion("type", [
+  z.object({
+    /** Source type */
+    type: z.literal("mp4"),
+    /** Video resolution (eg: 1080p) */
+    quality: quality,
+    /** Swarm reference of the video */
+    reference: beeReference,
+    /** Video size in bytes */
+    size: z.number().min(0),
+    /** Video bitrate */
+    bitrate: z.number().min(0).optional(),
+  }),
+  z.object({
+    /** Source type */
+    type: z.literal("dash"),
+    /** Path of the source */
+    path: z.string().min(3),
+  }),
+  z.object({
+    /** Source type */
+    type: z.literal("hls"),
+    /** Path of the source */
+    path: z.string().min(3),
+  }),
+])
 
-export const VideoRawSchema = z.object({
-  /**  Title of the video */
+export const VideoPreviewRawSchema = z.object({
+  /** Title of the video */
   title: slicedString(150),
-  /**  Description of the video */
-  description: slicedString(5000),
   /** Video creation timestamp */
   createdAt: z.number().min(0),
   /** Video creation timestamp */
   updatedAt: z.number().min(0).optional().nullable(),
-  /**  Quality of the original video */
-  originalQuality: quality,
-  /**  Address of the owner of the video */
+  /** Address of the owner of the video */
   ownerAddress: ethAddress,
-  /**  Duration of the video in seconds */
+  /** Duration of the video in seconds */
   duration: z.number().min(0),
   /** Thumbnail raw image */
   thumbnail: ImageRawSchema.nullable(),
-  /**  List of available qualities of the video */
+  /** Schema version */
+  v: z.enum(["1.0", "1.1", "1.2", "2.0"]).optional(),
+})
+
+export const VideoDetailsRawSchema = z.object({
+  /** Description of the video */
+  description: slicedString(5000),
+  /** Quality of the original video */
+  originalQuality: quality,
+  /** List of available qualities of the video */
   sources: z.array(VideoSourceRawSchema).min(1),
   /** batch id used */
   batchId: beeReference.nullable().optional(),
   /** Optional extra data */
   personalData: z.string().max(200).optional(),
   /** Schema version */
-  v: z.enum(["1.0", "1.1", "1.2"]).optional(),
+  v: z.enum(["1.0", "1.1", "1.2", "2.0"]).optional(),
 })
 
-export const VideoSourceSchema = VideoSourceRawSchema.extend({
-  /**  Source url */
-  source: z.string().url(),
-})
+export const VideoSourceSchema = z.discriminatedUnion("type", [
+  z.object({
+    /** Source type */
+    type: z.literal("mp4"),
+    /** Video resolution (eg: 1080p) */
+    quality: quality,
+    /** Swarm reference of the video */
+    reference: beeReference,
+    /** Video size in bytes */
+    size: z.number().min(0),
+    /** Video bitrate */
+    bitrate: z.number().min(0).optional(),
+    /** source url */
+    url: z.string().url(),
+  }),
+  z.object({
+    /** Source type */
+    type: z.literal("dash"),
+    /** Path of the source */
+    path: z.string().min(3),
+    /** source url */
+    url: z.string().url(),
+  }),
+  z.object({
+    /** Source type */
+    type: z.literal("hls"),
+    /** Path of the source */
+    path: z.string().min(3),
+    /** source url */
+    url: z.string().url(),
+  }),
+])
 
-export const VideoSchema = z.object({
-  /**  Hash of the video */
+export const VideoPreviewSchema = z.object({
+  /** Hash of the video */
   reference: beeReference,
-  /**  Title of the video */
+  /** Title of the video */
   title: slicedString(150),
-  /**  Description of the video */
-  description: slicedString(5000),
   /** Video creation timestamp */
   createdAt: z.number().min(0),
   /** Video update timestamp */
   updatedAt: z.number().min(0).nullable(),
-  /**  Quality of the original video */
-  originalQuality: quality,
-  /**  Address of the owner of the video */
+  /** Address of the owner of the video */
   ownerAddress: ethAddress,
-  /**  Duration of the video in seconds */
+  /** Duration of the video in seconds */
   duration: z.number().min(0),
-  /**  Thumbnail image data */
+  /** Thumbnail image data */
   thumbnail: ImageSchema.nullable(),
-  /**  All qualities of video */
+})
+
+export const VideoDetailsSchema = z.object({
+  /** Description of the video */
+  description: slicedString(5000),
+  /** Quality of the original video */
+  originalQuality: quality,
+  /** All qualities of video */
   sources: z.array(VideoSourceSchema).min(1),
   /** batch id used (null if v < `1.1`) */
   batchId: beeReference.nullable(),
@@ -75,6 +127,8 @@ export const VideoSchema = z.object({
 
 // Types
 export type VideoQuality = z.infer<typeof quality>
-export type VideoRaw = z.infer<typeof VideoRawSchema>
+export type VideoPreviewRaw = z.infer<typeof VideoPreviewRawSchema>
+export type VideoDetailsRaw = z.infer<typeof VideoDetailsRawSchema>
 export type VideoSource = z.infer<typeof VideoSourceSchema>
-export type Video = z.infer<typeof VideoSchema>
+export type VideoPreview = z.infer<typeof VideoPreviewSchema>
+export type VideoDetails = z.infer<typeof VideoDetailsSchema>
